67aa74bc2b21b226355c2cb06d3003058ebfbff7
Hanzzi <hans2734@LAP-1MTMMX1>
2017-01-06 14:40:42 +0100

    Updated Tic Tace Toe

--- Music/Music/TicTacToe.cs
+++ Music/Music/TicTacToe.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,8 @@ namespace Music
         public enum Player
         {
             X,
-            O
+            O,
+            Null
         }
 
         public enum PlayableCoords
@@ -31,32 +33,113 @@ namespace Music
         }
 
         // Contains the coordinates for all possible positions of pieces
-        public Dictionary<PlayableCoords, string> Coords;
+        public Dictionary<PlayableCoords, Player> Coords;
 
         public void AddCoords()
         {
-            Coords.Add(PlayableCoords.Coord11, null);
-            Coords.Add(PlayableCoords.Coord12, null);
-            Coords.Add(PlayableCoords.Coord13, null);
-            Coords.Add(PlayableCoords.Coord21, null);
-            Coords.Add(PlayableCoords.Coord22, null);
-            Coords.Add(PlayableCoords.Coord23, null);
-            Coords.Add(PlayableCoords.Coord31, null);
-            Coords.Add(PlayableCoords.Coord32, null);
-            Coords.Add(PlayableCoords.Coord33, null);
-        }    
+            Coords.Add(PlayableCoords.Coord11, Player.Null);
+            Coords.Add(PlayableCoords.Coord12, Player.Null);
+            Coords.Add(PlayableCoords.Coord13, Player.Null);
+            Coords.Add(PlayableCoords.Coord21, Player.Null);
+            Coords.Add(PlayableCoords.Coord22, Player.Null);
+            Coords.Add(PlayableCoords.Coord23, Player.Null);
+            Coords.Add(PlayableCoords.Coord31, Player.Null);
+            Coords.Add(PlayableCoords.Coord32, Player.Null);
+            Coords.Add(PlayableCoords.Coord33, Player.Null);
+        }
 
-        public async Task TicTacToeStart(CommandEventArgs e, string UserX, string UserO)
+        public async Task TicTacToeStart(CommandEventArgs e, string UserX, string UserO, DiscordClient _client)
         {
             Coords.Clear();
 
             AddCoords();
 
-            await e.Channel.SendMessage($"~  1 2 3{Environment.NewLine} 1 o x o {Environment.NewLine}2 x o x{Environment.NewLine}3 o x o");
+            await e.Channel.SendMessage("Welcome to Tic Tac Toe");
 
-            await e.Channel.SendMessage("Who will begin? X/O");
+            await e.Channel.SendMessage($"~  1 2 3{Environment.NewLine} 1 / / / {Environment.NewLine}2 / / /{Environment.NewLine}3 / / /");
 
-            // Make subscribe method to listen for who will start
+            await e.Channel.SendMessage("O plays first");
+
+            Player CurrentPlayer = Player.O;
+
+            _client.MessageReceived += ((s, m) =>
+            {
+                PlayableCoords Message;
+
+                Enum.TryParse(e.Message.Text, out Message);
+
+                if (CurrentPlayer == Player.O)
+                {
+                    foreach (KeyValuePair<PlayableCoords, Player> Coord in Coords)
+                    {
+                        if (Message == Coord.Key)
+                        {
+                            if (Coord.Value != Player.X)
+                                Coords[Coord.Key] = CurrentPlayer;
+                            Check(CurrentPlayer, e);
+                        }
+                    }
+                }
+                else if (CurrentPlayer == Player.X)
+                {
+                    foreach (KeyValuePair<PlayableCoords, Player> Coord in Coords)
+                    {
+                        if (Message == Coord.Key)
+                        {
+                            if (Coord.Value != Player.O)
+                                Coords[Coord.Key] = CurrentPlayer;
+                            Check(CurrentPlayer, e);
+                        }
+                    }
+                } else
+                {
+                    e.Channel.SendMessage("Something went wrong remember you can only enter X or O");
+                }
+                
+
+            });
         }
+
+        private bool Check(Player CurrentPlayer, CommandEventArgs e)
+        {
+
+            if (Coords[PlayableCoords.Coord11] == CurrentPlayer && Coords[PlayableCoords.Coord12] == CurrentPlayer && Coords[PlayableCoords.Coord13] == CurrentPlayer)
+            {
+                e.Channel.SendMessage($"{CurrentPlayer} Has Taken Coords 11, 12, 13 and has won the game");
+                return true;
+            }
+
+            if (Coords[PlayableCoords.Coord31] == CurrentPlayer && Coords[PlayableCoords.Coord32] == CurrentPlayer && Coords[PlayableCoords.Coord33] == CurrentPlayer)
+            {
+                e.Channel.SendMessage($"{CurrentPlayer} Has Taken Coords 31, 32, 33 and has won the game");
+                return true;
+            }
+
+            if (Coords[PlayableCoords.Coord11] == CurrentPlayer && Coords[PlayableCoords.Coord21] == CurrentPlayer && Coords[PlayableCoords.Coord31] == CurrentPlayer)
+            {
+                e.Channel.SendMessage($"{CurrentPlayer} Has Taken Coords 11, 21, 31 and has won the game");
+                return true;
+            }
+
+            if (Coords[PlayableCoords.Coord31] == CurrentPlayer && Coords[PlayableCoords.Coord32] == CurrentPlayer && Coords[PlayableCoords.Coord33] == CurrentPlayer)
+            {
+                e.Channel.SendMessage($"{CurrentPlayer} Has Taken Coords 31, 32, 33 and has won the game");
+                return true;
+            }
+
+            if (Coords[PlayableCoords.Coord11] == CurrentPlayer && Coords[PlayableCoords.Coord22] == CurrentPlayer && Coords[PlayableCoords.Coord33] == CurrentPlayer)
+            {
+                e.Channel.SendMessage($"{CurrentPlayer} Has Taken Coords 11, 22, 33 and has won the game");
+                return true;
+            }
+
+            if (Coords[PlayableCoords.Coord33] == CurrentPlayer && Coords[PlayableCoords.Coord22] == CurrentPlayer && Coords[PlayableCoords.Coord31] == CurrentPlayer)
+            {
+                e.Channel.SendMessage($"{CurrentPlayer} Has Taken Coords 33, 22, 31 and has won the game");
+                return true;
+            }
+            return false;
+        }
+        
     }
 }
